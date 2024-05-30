2bda944818d3c7e1e1af1bde5a39455d1f577f67
galang48 <galangyudha1945@gmail.com>
2024-05-30 10:52:13 +0700

    menangani kondisi antrian kosong

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -39,7 +39,11 @@ public:
     }
 
     void remove() {
-
+        // Cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
     }
 };
 int main()
